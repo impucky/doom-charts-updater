7465344385b19dcdf386b3ade89edfb7dfb02c3d
Raphaël Beaugiraud <raphael.bgrd@gmail.com>
2024-12-04 00:32:00 +0100

    filter album results

--- main.ts
+++ main.ts
@@ -9,34 +9,51 @@ const doomChartsUrl: string = Deno.args[0];
 const SPOTIFY_ID = Deno.env.get("SPOTIFY_ID");
 const SPOTIFY_SECRET = Deno.env.get("SPOTIFY_SECRET");
 
-if (!SPOTIFY_ID || !SPOTIFY_SECRET) {
-  console.error("Spotify client ID or secret is missing.");
+let descriptionString = "";
+
+function exit(message: string): never {
+  console.error(message);
   Deno.exit(1);
 }
 
+if (!SPOTIFY_ID || !SPOTIFY_SECRET)
+  exit("Spotify client ID or secret is missing.");
+
+// Get names
 const albumNames = await scrapeAlbums(doomChartsUrl);
-
-if (!albumNames) {
-  console.error("Failed to get the album names.");
-  Deno.exit(1);
-}
+if (!albumNames) exit("Failed to get the album names.");
 
 await confirm(chalk.magenta("Proceed with these albums?"));
 
+// Auth
 const clientToken = await getClientAuth(SPOTIFY_ID, SPOTIFY_SECRET);
+if (!clientToken) exit("Failed to get a client token.");
 
-if (!clientToken) {
-  console.error("Failed to get a client token.");
-  Deno.exit(1);
+// Get Ids
+let albumIds = await getAlbumIds(albumNames, clientToken);
+if (!albumIds) exit("Couldn't retrieve album ids");
+
+// Filter bad matches
+const excludePrompt = await prompt(
+  chalk.magenta("Choose matches to exclude: (comma separated)")
+);
+if (excludePrompt) {
+  // Parse prompt
+  const toExclude: number[] = excludePrompt.split(",").map(Number);
+  // Format description to "Missing: #1 ... | #2..."
+  descriptionString = `Missing: ${toExclude
+    .map(
+      (excludeIndex, index) =>
+        `#${excludeIndex} ${albumNames[excludeIndex - 1]} ${
+          index === toExclude.length - 1 ? "" : "| "
+        }`
+    )
+    .join("")}`;
+  // Filter ids
+  albumIds = albumIds.filter((_, i) => !toExclude.includes(i + 1));
 }
 
-const albumIds = await getAlbumIds(albumNames, clientToken);
-
-await confirm(chalk.magenta("Proceed with these matches?"));
-
 /** Todo next:
- * - Prompt to manually exclude bad matches
- * - Keep track of those as missing for playlist description
  * - Grab tracks for every album
  * - Spotify account auth
  * - Get all tracks in playlist
